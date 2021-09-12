e0552cc1014e3864f8123742e8413f8485a9bab5
Isaac <isaactij@sbcglobal.net>
2021-09-12 17:22:32 -0500

    TP-15 Added functionality for opening .txt files and saving to variable

---
+++ Thesis project 1.0/FileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thesis_project_1._0 {
+    class FileHandler {
+
+        public string GetInput() {
+            // Get users
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Choose .txt File";
+            openFileDialog.Filter = ".txt|*.txt";
+            string filePath = "";
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                filePath = openFileDialog.FileName;
+            }
+            // Open file to string
+            return System.IO.File.ReadAllText(filePath);
+        }
+
+    }
+}
