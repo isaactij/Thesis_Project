28c0a6491cfc738f8a92dfdbb7b967043634e669
Isaac <isaactij@sbcglobal.net>
2021-10-22 19:24:17 -0500

    Created Mobile Project

---
+++ Mobile Thesis Project 1.0/Thesis Project/Thesis Project/Thesis_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thesis_ProjectApp.swift
+//  Thesis Project
+//
+//  Created by Isaac Tijerina on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Thesis_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
