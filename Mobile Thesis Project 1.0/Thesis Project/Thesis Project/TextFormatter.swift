88d05e1d3b7e781876aa76115251a5c35072a729
Isaac <isaactij@sbcglobal.net>
2021-12-23 20:33:50 -0600

    Cleaned up commented code

--- Mobile Thesis Project 1.0/Thesis Project/Thesis Project/TextFormatter.swift
+++ Mobile Thesis Project 1.0/Thesis Project/Thesis Project/TextFormatter.swift
@@ -17,17 +17,14 @@ class TextFormatter {
     private var indexOfLastNewLine = -1
     
     func formatText(input: String) -> String{
-//        output = ""
         let specialWords = ["for", "less", "plus", "underscore", "to", "open", "quote", "close"]
         let singleDigitNumbers = ["one": "1"]
         let lowerCaseInput = input.lowercased()
         let dividedInput = lowerCaseInput.split(separator: " ")
         var forLoopBeingConstructed = false
-//        var forLoopDefinitionSection = 1
         var wordIndex = 0
         var quoteOpened = false
         var doNotAddPostSpace = false
-//        var addNewLine = false
         
         while wordIndex < dividedInput.count {
             var word = String(dividedInput[wordIndex])
@@ -56,8 +53,6 @@ class TextFormatter {
                     if(forLoopBeingConstructed) {
                         output += "..."
                     }
-//                case "print":
-//                    output += "print("
                 case "open":
                     nextWord = String(dividedInput[wordIndex + 1])
                     switch (nextWord) {
@@ -85,9 +80,6 @@ class TextFormatter {
                         nextWord = String(dividedInput[wordIndex + 2])
                         if(nextWord == "brace") {
                             output.removeLast()
-//                            output.lastIndex(where: {$0 == "\"" && $1 == "n"})
-//                            output.where
-//                            let index = output.index(output.startIndex, offsetBy: output.count - 2)
                             tabCount -= 1
                             let index = output.index(output.startIndex, offsetBy: indexOfLastNewLine)
                             let newLineSubstring = output[index...]
@@ -96,18 +88,11 @@ class TextFormatter {
                                 newLineHandler(tabOnly: false)
                             }
                             else if newLineSubstring.count > 2 {
-//                                let range = tabCount > 0 ? ((tabCount  * 4) - 1) : tabCount * 4
-//                                for _ in 0...range {
-//                                    output.removeLast()
-//                                }
                                 output = output.trimmingCharacters(in: .whitespaces)
                                 newLineHandler(tabOnly: true)
                             }
-//                            addNewLine = true
-//                            newLineHandler()
                             output += "}"
                             wordIndex += 2
-//                            tabCount -= 1
                             addNewLine = true
                         }
                     case "parenthesis":
@@ -134,34 +119,12 @@ class TextFormatter {
             } else {
                 output += word
             }
-//            if(forLoopBeingConstructed) {
-//                let number = Int(word)
-//                if(number != nil) {
-//                    if(forLoopDefinitionSection == 2) {
-//                        output += " {\n"
-//                    }
-//                    forLoopDefinitionSection += 1
-//                    if(forLoopDefinitionSection > 2) {
-//                        forLoopBeingConstructed = false
-//                        forLoopDefinitionSection = 1
-//                    }
-//                }
-//            }
             if !doNotAddPostSpace {
                 output += " "
             } else {
                 doNotAddPostSpace = false
             }
             newLineHandler(tabOnly: false)
-//            if addNewLine {
-//                output += "\n"
-//                if tabCount > 0 {
-//                    for _ in 1...tabCount {
-//                        output += "    "
-//                    }
-//                }
-//                addNewLine = false
-//            }
             wordIndex += 1
         }
         return output
